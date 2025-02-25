e65b6f3118908c5591c5f3b06df3b62c07d4cac4
Yashwanth <90yashwanth@gmail.com>
2025-02-26 00:21:48 +0530

    Delete proto.js

--- proto.js
+++
@@ -1,9 +0,0 @@
-function openVideo(images\video.mp4) {
-    let modal = document.getElementById("videoModal");
-    let videoElement = document.getElementById("modalVideo");
-
-    modal.style.display = "flex";
-    videoElement.src = images\video.mp4;
-    videoElement.controls = true; // Enable controls
-    videoElement.play(); // Start playback
-}
